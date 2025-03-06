c0bfa19369451f284e9567637c67d42a147a1364
nicolocostantino6 <nicolocostantino6@gmail.com>
2025-03-06 11:36:29 +0000

    ghange title

--- src/App.tsx
+++ src/App.tsx
@@ -35,7 +35,7 @@ export const App = () => {
            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
            onClick={() => setTitle("JOELTON") }>
             
-            Evolvi pokemon!
+            Evolvi pokemon!!!
           </button>
           <p className="text-center">
             Modifica <code>src/App.tsx</code> e salva per testare l'hot reload
