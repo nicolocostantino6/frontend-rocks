3c2280a0c643403c7bdcaf3fe9a34427acda00ac
nicolocostantino6 <nicolocostantino6@gmail.com>
2025-03-06 15:45:40 +0000

    tops

--- src/routes/root/index.tsx
+++ src/routes/root/index.tsx
@@ -1,56 +1,11 @@
-import { useState } from "react";
-import reactLogo from "../../assets/react.svg";
-import viteLogo from "/vite.svg";
-
-
- export const RootRoute =()=> {
-const [count, setCount] = useState(0);
-const[title,setTitle]= useState("EEVEE");
-return (
-  <div className="h-dvh flex flex-col items-center justify-center">
-    <div className="bg-white p-8 rounded-md shadow-lg">
-      <h1 className="text-center font-bold text-3xl text-blue-400 mb-4">
-        
-       {title}
-      </h1>
-
-      <div className="flex justify-center space-x-4 mb-2">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} alt="React logo" />
-        </a>
-      </div>
-
-      <h2 className="text-center font-bold text-xl mb-6">Vite + React</h2>
-
-      <div className="flex flex-col items-center space-y-4">
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Hai premuto il pulsante {count} {count === 1 ? "volta" : "volte"}
-        </button>
-        <button
-         className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
-         onClick={() => setTitle("JOELTON") }>
-          
-          Evolvi pokemon
-        </button>
-        <p className="text-center">
-          Modifica <code>src/App.tsx</code> e salva per testare l'hot reload
-      
-        </p>
-        <a href="/dettaglio/0">Link</a>
-      </div>
-      
-      <p className="text-center text-sm text-blue-400">Clicca sui loghi per saperne di più</p>
-    </div>
-  </div>
-);
+export const RootRoute =()=> {
+return <div>
+    {data.map((item)=>{
+        return <div>{item}</div>
+        })}
+</div>
 }
-
+const data =["bulbasaur","charmender","squirtle"];
 
 
 
