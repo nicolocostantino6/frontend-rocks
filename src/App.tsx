13cbf24639656f028efd7ee875bbf3c5a47dc273
nicolocostantino6 <nicolocostantino6@gmail.com>
2025-03-06 15:06:54 +0000

    modifica1

--- src/App.tsx
+++ src/App.tsx
@@ -35,13 +35,13 @@ export const App = () => {
            className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-md cursor-pointer hover:bg-blue-600 transition-colors"
            onClick={() => setTitle("JOELTON") }>
             
-            Evolvi pokemon!!!
+            Evolvi pokemon
           </button>
           <p className="text-center">
             Modifica <code>src/App.tsx</code> e salva per testare l'hot reload
           </p>
         </div>
-
+       <a href="/dettaglio/0">link</a>
         <p className="text-center text-sm text-blue-400">Clicca sui loghi per saperne di più</p>
       </div>
     </div>
---
+++ src/app/router/index.tsx
@@ -0,0 +1,13 @@
+import {BrowserRouter,Route,Routes}from "react-router";
+import {RootRoute}from "../../routes/root/index.tsx";
+import {DetailRoute}from "../../routes/detail/index.tsx";
+export const AppRouter=()=>{
+    return(
+    <BrowserRouter>
+    <Routes>
+    <Route path="/frontend-rocks" element={<RootRoute/>}/>
+    <Route path="/frontend-rocks/dettaglio/:id"element={<DetailRoute/>} />
+    </Routes>
+    </BrowserRouter>
+    )
+}
